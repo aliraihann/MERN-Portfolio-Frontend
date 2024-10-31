35b2c836b7788052697833b8fdd78bbb4351bf0e
aliraihann <aliraihann@gmail.com>
2024-10-31 18:45:34 +0700

    Update Tailwind config and rebuild

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx}",
     "./public/index.html"
   ],
-  mode: 'jit',
   theme: {
     backgroundImage: {
       'image': "url('/public/background.jpg')",
@@ -13,8 +13,6 @@ module.exports = {
       'sm': '640px',    // Small devices (mobile)
       'md': '768px',    // Medium devices (tablets)
       'lg': '1024px',   // Large devices (laptops)
-      // 'xl': '1280px',   // Extra large devices (desktops)
-      // '2xl': '1536px'   // Extra extra large devices
     }
   },
   plugins: [],
