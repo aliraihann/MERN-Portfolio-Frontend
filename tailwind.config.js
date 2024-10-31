e01f6743cba3a1d9e7f1332b2b8a4f634a2f8967
aliraihann <aliraihann@gmail.com>
2024-10-31 11:13:07 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,8 @@ module.exports = {
       'sm': '640px',    // Small devices (mobile)
       'md': '768px',    // Medium devices (tablets)
       'lg': '1024px',   // Large devices (laptops)
-      'xl': '1280px',   // Extra large devices (desktops)
-      '2xl': '1536px'   // Extra extra large devices
+      // 'xl': '1280px',   // Extra large devices (desktops)
+      // '2xl': '1536px'   // Extra extra large devices
     }
   },
   plugins: [],
