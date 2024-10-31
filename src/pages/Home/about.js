16d1cad32124ad823f5c53b796028799ebd08777
aliraihann <aliraihann@gmail.com>
2024-10-31 11:09:23 +0700

    update about.js

--- src/pages/Home/about.js
+++ src/pages/Home/about.js
@@ -9,26 +9,26 @@ function About() {
   return (
     <div id="about-section" className="h-screen">
       <div className="flex lg:w-full">
-        <div className="w-1/2 flex flex-col">
-          <h1 className="font-graffiti-caption lg:text-2xl text-left pl-10 text-blue-700">
+        <div className="flex flex-col lg:w-1/2">
+          <h1 className="font-graffiti-caption text-left text-blue-700 lg:text-9xl  lg:pl-10 ">
             From <br />
             Back Office <br />
             to <br />
             Breakthrough Ideas!
           </h1>
         </div>
-        <div className="flex flex-col w-1/2">
-          <p className="font-graffiti pt-10 lg:text-3xl text-black">
+        <div className="flex flex-col lg:w-1/2">
+          <p className="font-graffiti text-black lg:text-3xl lg:pt-10">
             {description1 || "  "}
           </p>
           <div className="flex flex-col">
             <h1 className="font-graffiti justify-end text-black lg:text-3xl">
               Here are a few technologies I've been working recently:
             </h1>
-            <div className="flex flex-wrap justify-center gap-10 mb-11">
+            <div className="flex flex-wrap justify-center gap-10 lg:mb-11">
               {skills.map((skill, index) => (
                 <div className="">
-                  <h1 className="font-graffiti text-4xl -rotate-2 text-black bg-red-600">
+                  <h1 className="font-graffiti text-black bg-red-600 lg:text-4xl lg:-rotate-2">
                     {skill}
                   </h1>
                 </div>
