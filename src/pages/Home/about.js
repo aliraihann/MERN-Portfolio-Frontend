54b81d413508a5520107a44d1982ed176a5f6dfd
aliraihann <aliraihann@gmail.com>
2024-11-01 14:53:39 +0700

    update intro.js and about.js

--- src/pages/Home/about.js
+++ src/pages/Home/about.js
@@ -7,7 +7,7 @@ function About() {
   const { description1, skills } = about;
 
   return (
-    <section id="about-section" className="min-h-screen overflow-hidden py-8 px-4">
+    <section id="about-section" className="min-h-screen overflow-hidden lg:pt-1 px-4">
       <div className="max-w-8xl mx-auto flex flex-col lg:flex-row gap-8">
         {/*left column */}
         <div className="w-full lg:w-1/2">
@@ -27,10 +27,10 @@ function About() {
             <h1 className="font-graffiti justify-end text-xl lg:text-2xl text-black">
               Here are a few technologies I've been working recently:
             </h1>
-            <div className="flex flex-wrap justify-center gap-5">
+            <div className="flex flex-wrap justify-center gap-3 m-0">
               {skills.map((skill, index) => (
                 <div key={index} >
-                  <h1 className="font-graffiti text-xl lg:text-2xl lg:-rotate-2  text-black bg-red-600">
+                  <h1 className="font-graffiti text-xl lg:text-2xl lg:-rotate-2 mt-0 text-black bg-red-600/90">
                     {skill}
                   </h1>
                 </div>
--- src/pages/Home/intro.js
+++ src/pages/Home/intro.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function Intro() {
-
   const handleButtonClick = () => {
     const aboutElement = document.getElementById("about-section");
     if (aboutElement) {
@@ -10,17 +9,22 @@ function Intro() {
   };
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-10">
-      <div className="bg-white box-content h-[450px] w-[500px] rounded-2xl border-4 border-black p-2">
-        <div className="bg-red-600 box-content h-full flex flex-col rounded-2xl">
-          <h1 className="text-white text-9xl text-center font-bold m-0">HI!</h1>
-          <h1 className="text-white text-6xl text-center font-bold m-0">
-            MY NAME IS
-          </h1>
-          <div className="bg-white flex-grow flex items-center justify-center mb-8">
-            <h1 className="font-graffiti text-9xl text-black mt-12 tracking- text-nowrap -rotate-6">
-              ALI RAIHAN
+    <section className="min-h-screen flex flex-col justify-center items-center py-4 px-4">
+      {/* Main Card Container */}
+      <div className="relative w-full max-w-lg mx-auto  my-auto">
+        <div className="bg-white rounded-2xl lg:h-[450px] lg:w-[450px] border-4 border-black p-1">
+          <div className="bg-red-600 box-content h-full flex flex-col rounded-2xl">
+            <h1 className="text-white text-9xl text-center font-bold m-0">
+              HI!
             </h1>
+            <h1 className="text-white text-6xl text-center font-bold m-0">
+              MY NAME IS
+            </h1>
+            <div className="bg-white flex-grow flex items-center justify-center mb-8">
+              <h1 className="font-graffiti lg:text-8xl text-black mt-12 tracking- text-nowrap -rotate-6">
+                ALI RAIHAN
+              </h1>
+            </div>
           </div>
         </div>
       </div>
@@ -30,7 +34,7 @@ function Intro() {
       >
         Let's get started!
       </button>
-    </div>
+    </section>
   );
 }
 
